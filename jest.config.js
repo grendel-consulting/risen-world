03de634fa8d4600e1b5189b343f9e51ff90acebb
ramirezj <ramirezj@users.noreply.github.com>
2021-03-26 21:40:28 +0000

    build: tune coverage report

--- jest.config.js
+++ jest.config.js
@@ -15,7 +15,7 @@ module.exports = {
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
     ...paths,
   },
-  testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`, `<rootDir>gatsby.*js`, `<rootDir>jest.*js`],
+  testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`, `<rootDir>/gatsby.*js`, `<rootDir>/jest.*js`],
   transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
   globals: {
     __PATH_PREFIX__: ``,
@@ -27,6 +27,8 @@ module.exports = {
     "**/*.{js,jsx,ts,tsx}",
     "!**/.cache/**",
     "!**/coverage/**",
+    "!**/gatsby*.js",
+    "!**/jest*.js",
     "!**/node_modules/**",
     "!**/public/**",
     "!**/vendor/**"
